6865a2005de00fab9ca41348ca1f2e738416e674
Kieran <kiermccarth+github@gmail.com>
2024-04-18 19:12:31 +0100

    ADD: Use index.scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: "en",
-      },
-      link: [
-        {
-          rel: "icon",
-          type: "image/svg+xml",
-          href: "./assets/meta/favicon.svg",
-        },
-        // {
-        //   rel: "stylesheet",
-        //   href: "https://fonts.googleapis.com/css?family=FONTS+TO+LOAD&display=swap",
-        // },
-      ],
-    },
-  },
-  devtools: { enabled: false },
-  nitro: {
-    compressPublicAssets: true,
-  },
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: "en",
+			},
+			link: [
+				{
+					rel: "icon",
+					type: "image/svg+xml",
+					href: "./assets/meta/favicon.svg",
+				},
+				// {
+				//   rel: "stylesheet",
+				//   href: "https://fonts.googleapis.com/css?family=FONTS+TO+LOAD&display=swap",
+				// },
+			],
+		},
+	},
+	css: ["~/assets/styles/index.scss"],
+	devtools: { enabled: false },
+	nitro: {
+		compressPublicAssets: true,
+	},
 });
