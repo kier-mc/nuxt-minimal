df7d7f2013e8432af3d8c918c5119c23e07dccaf
Kieran <kiermccarth+github@gmail.com>
2024-04-30 14:10:24 +0100

    ADD: Add @nuxt/eslint to modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: "en",
-			},
-			link: [
-				{
-					rel: "icon",
-					type: "image/svg+xml",
-					href: "./assets/meta/favicon.svg",
-				},
-				// {
-				//   rel: "stylesheet",
-				//   href: "https://fonts.googleapis.com/css?family=FONTS+TO+LOAD&display=swap",
-				// },
-			],
-		},
-	},
-	css: ["~/assets/styles/index.scss"],
-	devtools: { enabled: false },
-	nitro: {
-		compressPublicAssets: true,
-	},
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "./assets/meta/favicon.svg",
+        },
+        // {
+        //   rel: "stylesheet",
+        //   href: "https://fonts.googleapis.com/css?family=FONTS+TO+LOAD&display=swap",
+        // },
+      ],
+    },
+  },
+  css: ["~/assets/styles/index.scss"],
+  devtools: { enabled: false },
+  modules: ["@nuxt/eslint"],
+  nitro: {
+    compressPublicAssets: true,
+  },
 });
